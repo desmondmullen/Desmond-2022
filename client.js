b7142ce412a5b1aebe5d6ce05611f98e35578c1d
Desmond Mullen <desmond@cell-ed.com>
2024-09-18 16:39:09 -0400

    adds message signing

--- client.js
+++ client.js
@@ -10,8 +10,15 @@ app.use(express.json());
 let client01PasswordSalt = crypto.randomBytes(128).toString('base64');
 let client01Password;
 
+let client01PublicKey;
 let client01PrivateKeySalt = crypto.randomBytes(128).toString('base64');
 let client01PrivateKey;
+
+function signMessage(message) {
+  return crypto.createHmac('sha256', client01PrivateKey)
+    .update(message)
+    .digest('hex');
+}
 
 // on startup, we hash the password from the command line and store it in memory (in lieu of a real database)
 client01Password = crypto.createHmac('sha256', process.argv[2])
@@ -31,17 +38,24 @@ app.post('/set-public-key', (req, res) => {
   client01PrivateKey = crypto.createHmac('sha256', publicKey)
     .update(client01PrivateKeySalt)
     .digest('hex');
+  client01PublicKey = publicKey;
   res.send({ publicKeySet: true });
+});
+
+// then we wait for a message to sign
+app.post('/sign-message', (req, res) => {
+  const { message } = req.body;
+  const signature = signMessage(message);
+  console.log(`message: ${message}\nsignature: ${signature}`);
+  res.send({ message, signature });
 });
 
 // then we wait for a signed message to verify
 app.post('/verify-message', (req, res) => {
   const { message, signature } = req.body;
-  // compare the signature to the private key
-  const messageValidated = crypto.createHmac('sha256', signature)
-    .update(client01PrivateKeySalt)
-    .digest('hex') === client01PrivateKey;
-  res.send({ message, messageValidated });
+  const valid = signMessage(message) === signature;
+  console.log(`message: ${message}\nsignature: ${signature}\nvalid: ${valid}`);
+  res.send({ message, signature, valid });
 });
 
 app.listen(port, () => {
